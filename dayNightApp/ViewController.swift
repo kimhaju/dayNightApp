4aa24d4abd14163408b4d74b18e5860fc5bf209a
kim ha-ju <haju0321@gmail.com>
2021-07-16 22:45:10 +0900

    Initial Commit

---
+++ dayNightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dayNightApp
+//
+//  Created by haju on 2021/07/16.
+//  Copyright © 2021 haju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
