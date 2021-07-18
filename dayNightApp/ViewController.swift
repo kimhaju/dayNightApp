428063fcddb90256e98a494b6a40ad06d341ae44
kim ha-ju <haju0321@gmail.com>
2021-07-18 10:16:12 +0900

    my dayNightApp

--- dayNightApp/ViewController.swift
+++ dayNightApp/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let dayImg = UIImage(named: "day.jpg")
+    let nightImg = UIImage(named: "night.jpg")
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var stateSwitch: UISwitch!
+    @IBOutlet weak var dayNightImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dayNightImg.image = dayImg
+        
+        stateSwitch.addTarget(self, action: #selector(stateChanged), for: .valueChanged)
     }
-
-
+   @objc func stateChanged(switchState: UISwitch) {
+          if switchState.isOn {
+              textLabel.text = "Day Mode"
+            dayNightImg.image = dayImg
+            textLabel.textColor = UIColor.white
+           
+          } else {
+              textLabel.text = "Night Mode"
+            dayNightImg.image = nightImg
+            textLabel.textColor = UIColor.cyan
+           
+          }
+      }
 }
 
